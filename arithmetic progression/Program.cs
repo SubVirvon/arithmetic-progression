000453dd765f1bce30db28e7f6c8cd02824c5e6b
Virvon <dan130205@gmail.com>
2022-09-30 10:18:36 +0300

    arithmetic progression

---
+++ arithmetic progression/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arithmetic_progression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 5; i <= 96; i += 7)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
